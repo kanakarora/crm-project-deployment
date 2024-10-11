b675d4c23e711bc7b2c3c97ffad9b80196649613
kanika <kanakarora98@gmail.com>
2024-10-11 14:02:26 +0530

    Task page updated

--- src/Admin/pages/Tasks/Task.jsx
+++ src/Admin/pages/Tasks/Task.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 const Task = () => {
+
+    const navigate = useNavigate();
+   
+
     const [openIndex, setOpenIndex] = useState(null);
 
     // Function to handle accordion toggling
@@ -21,46 +26,46 @@ const Task = () => {
                 <div>
                     <table className="w-full">
                         <thead>
-                        <tr>
-                           
-                            <th className="text-[12px] flex items-center gap-[10px] font-medium p-[10px] w-[116px] border-r whitespace-nowrap"><span className="todo-btn">To Do</span><span className="six-tasks bg-[red]">6</span></th>
+                            <tr>
+
+                                <th className="text-[12px] flex items-center gap-[10px] font-medium p-[10px] w-[116px] border-r whitespace-nowrap"><span className="todo-btn">To Do</span><span className="six-tasks bg-[red]">6</span></th>
 
 
-                            <th className="text-[12px] border-r w-[40px]  font-medium p-[8px] ">#</th>
+                                <th className="text-[12px] border-r w-[40px]  font-medium p-[8px] ">#</th>
 
-                            
-                            <th className="text-[12px] w-[250px] p-[10px] border-r font-medium whitespace-nowrap">Task Name</th>
 
-                           
-
+                                <th className="text-[12px] w-[250px] p-[10px] border-r font-medium whitespace-nowrap">Task Name</th>
 
 
 
 
 
-                            <th className="text-[12px] font-medium p-[10px] w-[100px] border-r whitespace-nowrap	">Start Date</th>
-
-
-                            <th className="text-[12px] font-medium p-[10px] w-[100px] border-r whitespace-nowrap	">Due Date</th>
-                            <th className="text-[12px] font-medium p-[10px] w-[100px] border-r whitespace-nowrap	">End Date</th>
-
-
-                            <th className="text-[12px] font-medium p-[10px] w-[120px] border-r whitespace-nowrap	">Assigned to</th>
-
-
-                            <th className="text-[12px] w-[100px] font-medium p-[10px] border-r whitespace-nowrap	">Tags</th>
-
-
-                            <th className="text-[12px] w=[90px] font-medium p-[10px]  whitespace-nowrap	">Priority</th>
 
 
 
-                           
+                                <th className="text-[12px] font-medium p-[10px] w-[100px] border-r whitespace-nowrap	">Start Date</th>
 
-                            
 
-                        </tr>
-                        
+                                <th className="text-[12px] font-medium p-[10px] w-[100px] border-r whitespace-nowrap	">Due Date</th>
+                                <th className="text-[12px] font-medium p-[10px] w-[100px] border-r whitespace-nowrap	">End Date</th>
+
+
+                                <th className="text-[12px] font-medium p-[10px] w-[120px] border-r whitespace-nowrap	">Assigned to</th>
+
+
+                                <th className="text-[12px] w-[100px] font-medium p-[10px] border-r whitespace-nowrap	">Tags</th>
+
+
+                                <th className="text-[12px] w=[90px] font-medium p-[10px]  whitespace-nowrap	">Priority</th>
+
+
+
+
+
+
+
+                            </tr>
+
                         </thead>
                     </table>
                 </div>,
@@ -68,115 +73,99 @@ const Task = () => {
                 <table className="w-full" >
                     <tbody>
 
-                    <tr>
-                        <td className="flex p-[0]">
+                        <tr>
+                            <td className="flex p-[0]">
 
-                        <td className="p-[10px] w-[108px]"><Link className="text-[10px]  text-[#47cc00] " to="/">Complete</Link></td>
+                                <td className="p-[10px] w-[108px]"><Link className="text-[10px]  text-[#47cc00] " >Complete</Link></td>
 
-                            <td className="text-[12px] w-[40px] font-medium p-[10px] text-[#2563eb] hover:text-blue-950">43</td>
-                           
-
-
-                            <td className="flex flex-col w-[250px] p-[10px] break-words"><Link className="text-[11px] text-[#2563eb]" to="/taskview">soul relation intro</Link> <Link className="text-[9px] text-[#000]" to="/">#12 - DIVINE HEALING-AUG-2024 - DIVINE HEALING</Link></td>
+                                <td className="text-[12px] w-[40px] font-medium p-[10px] text-[#2563eb] hover:text-blue-950">43</td>
 
 
 
-                          
-                            <td className="text-[12px] p-[10px] w-[100px] whitespace-nowrap	">13-08-2024</td>
-
-                            <td className="text-[12px] p-[10px]  w-[100px]   whitespace-nowrap	">13-08-2024</td>
-                            <td className="text-[12px] p-[10px] w-[100px] whitespace-nowrap	">19-08-2024</td>
-
-                            <td className="w-[120px]"></td>
-
-                            <td className="p-[10px] w-[100px]"><Link className="text-[12px] pl-[7px] pr-[7px] pb-[5px] pt-[5px] text-[#000] rounded-md border border-3 border-[#e2e8f0]" to="/">Ads</Link></td>
-
-                            <td className="text-[#ff6f00] p-[10px] w-[100px]  text-[12px] whitespace-nowrap	">High</td>
+        <td onClick={() => navigate('/taskview')} className="flex flex-col w-[250px] p-[10px] break-words">
+        <p  className="text-[11px] text-[#2563eb]">soul relation intro</p>
+        <p className="text-[9px] text-[#000]"> #12 - DIVINE HEALING-AUG-2024 - DIVINE HEALING</p>
+     </td>
 
 
 
-                        </td>
+        <td className="text-[12px] p-[10px] w-[100px] whitespace-nowrap	">13-08-2024</td>
 
+        <td className="text-[12px] p-[10px]  w-[100px]   whitespace-nowrap	">13-08-2024</td>
+        <td className="text-[12px] p-[10px] w-[100px] whitespace-nowrap	">19-08-2024</td>
 
-                    </tr>
-                    </tbody>
+        <td className="w-[120px]"></td>
 
-                    <tr>
-                        <td className="flex p-[0]">
+        <td className="p-[10px] w-[100px]"><Link className="text-[12px] pl-[7px] pr-[7px] pb-[5px] pt-[5px] text-[#000] rounded-md border border-3 border-[#e2e8f0]" to="/">Ads</Link></td>
 
-                        <td className="p-[10px] w-[108px]"><Link className="text-[10px]  text-[#47cc00] " to="/">Complete</Link></td>
+        <td className="text-[#ff6f00] p-[10px] w-[100px]  text-[12px] whitespace-nowrap	">High</td>
 
-                            <td className="text-[12px] w-[40px] font-medium p-[10px] text-[#2563eb] hover:text-blue-950">143</td>
-                           
+  </td>
+</tr>
+</tbody>
 
+          <tr>
+          <td className="flex p-[0]">
 
-                            <td className="flex flex-col w-[250px] p-[10px] break-words"><Link className="text-[11px] text-[#2563eb]" to="/taskview">URBAN KHALSA REEL</Link> <Link className="text-[9px] text-[#000]" to="/">#8 - URBAN KHALSA - URBAN KHALSA</Link></td>
+        <td className="p-[10px] w-[108px]"><Link className="text-[10px]  text-[#47cc00] " to="/">Complete</Link></td>
 
-
-
-                          
-                            <td className="text-[12px] p-[10px] w-[100px] whitespace-nowrap	">16-08-2024</td>
-
-                            <td className="text-[12px] p-[10px]  w-[100px]   whitespace-nowrap	">18-08-2024</td>
-                            <td className="text-[12px] p-[10px] w-[100px] whitespace-nowrap	">20-08-2024</td>
-
-                            <td className="w-[120px]"></td>
-
-                            <td className="p-[10px] w-[100px]"><Link className="text-[12px] pl-[7px] pr-[7px] pb-[5px] pt-[5px] text-[#000] rounded-md border border-3 border-[#e2e8f0]" to="/">Ads</Link></td>
-
-                            <td className="text-[#2563eb] p-[10px] w-[100px]  text-[12px] whitespace-nowrap	">Medium</td>
+        <td className="text-[12px] w-[40px] font-medium p-[10px] text-[#2563eb] hover:text-blue-950">143</td>
 
 
 
-                        </td>
+        <td className="flex flex-col w-[250px] p-[10px] break-words">
+        <p className="text-[11px] text-[#2563eb]" onClick={() => navigate("/taskview")}>URBAN KHALSA REEL</p>
+        <p className="text-[9px] text-[#000]" to="/">#8 - URBAN KHALSA - URBAN KHALSA</p>
+        </td>
 
 
-                    </tr>
 
-                
 
-                </table>
+        <td className="text-[12px] p-[10px] w-[100px] whitespace-nowrap	">16-08-2024</td>
+
+        <td className="text-[12px] p-[10px]  w-[100px]   whitespace-nowrap	">18-08-2024</td>
+        <td className="text-[12px] p-[10px] w-[100px] whitespace-nowrap	">20-08-2024</td>
+
+        <td className="w-[120px]"></td>
+
+        <td className="p-[10px] w-[100px]"><Link className="text-[12px] pl-[7px] pr-[7px] pb-[5px] pt-[5px] text-[#000] rounded-md border border-3 border-[#e2e8f0]" to="/">Ads</Link></td>
+
+        <td className="text-[#2563eb] p-[10px] w-[100px]  text-[12px] whitespace-nowrap	">Medium</td>
+ </td>
+</tr>
+</table>
 
             )
         },
-
-        //   { title: "2", content: "This is the content for item 2" },
-        //     { title: "3", content: "This is the content for item 3" },
-
     ];
     return (
-        <div className=" w-full absolute pl-[140px] top-[95px] right-[5px] ">
+    <div className=" w-full absolute pl-[140px] top-[95px] right-[5px] ">
 
-            <div className="bg-[#fff] p-[10px] pl-[100px]">
-                <div className="mb-[14px]">
+    <div className="bg-[#fff] p-[10px] pl-[100px]">
+   <div className="mb-[14px]">
 
-                    <Link to="/newtask" className="text-[#fff] text-[14px] bg-[#8a25b0] mb-[10px]  focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg  p-[8px] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <Link to="/newtask" className="text-[#fff] text-[14px] bg-[#8a25b0] mb-[10px]  focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg  p-[8px] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <AddIcon /> New Task</Link>
                 </div>
-                {accordionItems.map((item, index) => (
-                    <div key={index} className="">
+    {accordionItems.map((item, index) => (
+    <div key={index} className="">
                         {/* Accordion Header */}
-                        <button
-                            onClick={() => handleToggle(index)}
-                            className="flex justify-between items-center w-full text-left text-gray-800 shadow-lg  rounded-lg focus:outline-none"
-                        >
-                            <span>{item.title}</span>
-                            <span>{openIndex === index ? "" : ""}</span>
-                        </button>
+   <button onClick={() => handleToggle(index)}
+    className="flex justify-between items-center w-full text-left text-gray-800 shadow-lg  rounded-lg focus:outline-none"
+    >
+    <span>{item.title}</span>
+    <span>{openIndex === index ? "" : ""}</span>
+    </button>
 
                         {/* Accordion Content */}
-                        {openIndex === index && (
-                            <div className="p-[8px] mb-[10px] text-gray-700 bg-white">
-                                {item.content}
-
-                            </div>
-                        )}
-                    </div>
-                ))}
-
-
-            </div>
-        </div>
+    {openIndex === index && (
+    <div className="p-[8px] mb-[10px] text-gray-700 bg-white">
+    {item.content}
+   </div>)}
+</div>
+))}
+</div>
+</div>
     );
 };
 
